819f14f84b5fbc8ff3fcd0db1e846c215890bc8d
Unluckii9 <calviereelie9@gmail.com>
2025-05-31 01:39:28 +0200

    temp

--- commands/accouts.py
+++ commands/accouts.py
@@ -18,11 +18,31 @@ def register(tree: app_commands.CommandTree):
             color=discord.Color.blue()
         )
 
+        user_mails = []
+        user_passwords = []
+
         for user_account in user_accounts:
             mail = user_account[0]
             password = user_account[1]
 
-            embed.description += f"{mail} | {password}\n"
+            user_mails.append(mail)
+            user_passwords.append(password)
+        
+        account = Account()
+        await interaction.response.send_message(embed=embed, view=account)
 
-        await interaction.response.send_message(embed=embed)
+class Account(discord.ui.View):
+    def __init__(self):
+        super().__init__(timeout=None)
 
+        self.index_user_account = 0
+
+    @discord.ui.button(label="Précédent", style=discord.ButtonStyle.danger, custom_id="precedent_btn")
+    async def valider(self, interaction: Interaction, button: discord.ui.Button):
+        await interaction.response.send_message("✅ Tu as cliqué sur Valider !", ephemeral=True)
+
+    @discord.ui.button(label="Suivant", style=discord.ButtonStyle.success, custom_id="suivant_btn")
+    async def annuler(self, interaction: Interaction, button: discord.ui.Button):
+        await interaction.response.send_message("❌ Action annulée.", ephemeral=True)
+
+    
